f9bb7d7869d8eb0c6ca2a9c60b45de4cb86ed232
Muluken Zemed <zmmulu@gmail.com>
2023-09-02 20:10:07 +0300

    Add Book component

---
+++ src/components/Book.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Book = (props) => {
+  const { title, author } = props;
+  return (
+    <div>
+      <h2>{title}</h2>
+      <h3>{author}</h3>
+    </div>
+  );
+};
+
+Book.propTypes = {
+  title: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+};
+
+export default Book;
