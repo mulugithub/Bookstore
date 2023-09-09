50d0bc926c9993bf2ab9df707afbea98eccdb64a
Muluken Zemed <zmmulu@gmail.com>
2023-09-09 12:54:55 +0300

    Refactor add book feature

--- src/components/AddBook.jsx
+++ src/components/AddBook.jsx
@@ -1,25 +1,35 @@
 import React, { useState } from 'react';
-import { uuid } from 'uuidv4';
+import { v4 as uuidv4 } from 'uuid';
 import { useDispatch } from 'react-redux';
-import { addBook } from '../redux/books/booksSlice';
+import { addBook, getBooks } from '../redux/books/booksSlice';
 
 const AddBook = () => {
   const dispatch = useDispatch();
   const [title, setTitle] = useState('');
   const [author, setAuthor] = useState('');
+  const [category, setCategory] = useState('');
 
   const addBookToList = (e) => {
     e.preventDefault();
-    dispatch(addBook({
-      id: uuid,
+    // const uniqueID = ;
+    // console.log('uniqueID', uniqueID);
+    const book = {
+      item_id: uuidv4(),
       title,
       author,
-      category: 'Fiction',
-    }));
-
+      category,
+    };
+    dispatch(addBook(book)).then(() => {
+      dispatch(getBooks());
+    });
     setTitle('');
     setAuthor('');
+    setCategory('');
   };
+
+  const Categories = [
+    'Fiction', 'Nonfiction', 'Mystery', 'Novel', 'Thriller', 'Graphic Novels', 'Other',
+  ];
 
   return (
     <section>
@@ -27,6 +37,12 @@ const AddBook = () => {
         <div>
           <input type="text" name="title" id="name" value={title} placeholder="Title" onChange={(e) => setTitle(e.target.value)} required />
           <input type="text" name="author" id="author" value={author} placeholder="Author" onChange={(e) => setAuthor(e.target.value)} required />
+          <select value={category} onChange={(e) => setCategory(e.target.value)} required>
+            <option value="" disabled>Select a category</option>
+            {Categories.map((categorie) => (
+              <option key={categorie} value={categorie}>{categorie}</option>
+            ))}
+          </select>
           <button type="submit"> Add Book </button>
         </div>
       </form>
