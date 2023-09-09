740e1016e78d7ff8c859902ab408d77969d578a9
Muluken Zemed <zmmulu@gmail.com>
2023-09-09 19:03:34 +0300

    Add style for Navbar component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FaUser } from 'react-icons/fa6';
 
 const Navbar = () => (
-  <div className="Navbar">
-    <header>
-      <nav>
-        <div className="logo">Bookstore CMS</div>
-        <ul>
-          <li>
-            <Link to="/">Books</Link>
-          </li>
-          <li>
-            <Link to="/Categories"> Categories</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  </div>
+  <header>
+    <nav>
+      <div className="logo">Bookstore CMS</div>
+      <ul>
+        <li>
+          <Link to="/">Books</Link>
+        </li>
+        <li>
+          <Link to="/Categories"> Categories</Link>
+        </li>
+      </ul>
+      <div className="user-icon oval"><FaUser /></div>
+    </nav>
+  </header>
 );
 
 export default Navbar;
